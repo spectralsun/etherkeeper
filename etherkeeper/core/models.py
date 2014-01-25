1ea1f2d75b4fb0fc3075b213c636bfc8830f8637
spectralsun <garrett@spectralsun.com>
2014-01-25 15:57:03 -0800

    added user post_save listener

--- etherkeeper/core/models.py
+++ etherkeeper/core/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.db.models.signals import post_save
+from etherkeeper.util.helpers import get_etherpad_client
 
 class Author(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, related_name='author')
     etherpad_id = models.CharField(max_length=42)
 
     @classmethod
@@ -19,3 +20,14 @@ class Author(models.Model):
 
     def get_documents(self):
         return self.pads.all()
+
+def on_user_save(sender, instance, **kwargs):
+    if not Author.get_by_user(instance):
+        e = get_etherpad_client()
+        author = Author(user=instance)
+        author.etherpad_id = e.createAuthor(name=instance.username)['authorID']
+        author.save()
+
+
+
+post_save.connect(on_user_save, sender=User)
