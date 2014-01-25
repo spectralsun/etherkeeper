39fefc15fed791aded1e0666b97ae9d52e584f96
spectralsun <garrett@spectralsun.com>
2014-01-24 19:31:56 -0800

    added tagsinput and typeahead

--- etherkeeper/core/assets.py
+++ etherkeeper/core/assets.py
@@ -6,6 +6,8 @@ js = Bundle(
     'lib/jquery/jquery.cookie.js',
     'lib/alertify/alertify.min.js',
     'lib/bootstrap/bootstrap.min.js',
+    'lib/bootstrap/bootstrap-tagsinput.min.js',
+    'lib/typeahead.js',
     'lib/lodash.min.js',
     'lib/davis.min.js',
     'js/etherkeeper.js'
@@ -14,8 +16,10 @@ register('js', js)
 
 css = Bundle(
     'lib/bootstrap/bootstrap.min.css',
+    'lib/bootstrap/bootstrap-tagsinput.css',
     'lib/alertify/alertify.core.css',
     'lib/alertify/alertify.default.css',
+    'lib/typeahead.css',
     'css/etherkeeper.css',
     'css/bootstrap.css',
 )
