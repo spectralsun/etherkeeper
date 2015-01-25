71c28af18cbef1fb9b91926cda6f5f5104e0d4a0
spectralsun <garrett@spectralsun.com>
2015-01-24 17:39:39 -0800

    settings.py: clean up apps

--- etherkeeper/settings.py
+++ etherkeeper/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'south',
+
+    'django_assets',
+    'django_jinja2',
+
+    'etherkeeper',
     'etherkeeper.core',
     'etherkeeper.etherpad',
     'etherkeeper.organize',
-    'django_assets',
-    'django_jinja2',
 )
 
 MIDDLEWARE_CLASSES = (
