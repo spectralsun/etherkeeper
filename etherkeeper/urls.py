7095bea450d6ee81211ce7e1d2aaa20e6a27b914
spectralsun <garrett@spectralsun.com>
2015-01-24 17:37:55 -0800

    urls.py: remove ending etherpad slash

--- etherkeeper/urls.py
+++ etherkeeper/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     url(r'^logout', 'etherkeeper.core.views.logout_view', name='logout'),
     url(r'^search/users', 'etherkeeper.core.views.search_users_view', name='logout'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^etherpad/', include('etherkeeper.etherpad.urls'))
+    url(r'^etherpad', include('etherkeeper.etherpad.urls')),
 
     url(r'.*', 'etherkeeper.core.views.home_view', name='home'),
 )
