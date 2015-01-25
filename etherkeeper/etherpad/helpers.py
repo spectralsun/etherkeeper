0822d176a210224d900e7f42b5ec0c0d632e69e1
spectralsun <garrett@spectralsun.com>
2015-01-24 18:02:23 -0800

    etherpad helpers: add comments and clean up

--- etherkeeper/etherpad/helpers.py
+++ etherkeeper/etherpad/helpers.py
@@ -2,23 +2,33 @@ from django.conf import settings
 from etherpad_lite import EtherpadLiteClient
 from etherkeeper.util.helpers import jsonify, set_cookie
 
+
 def get_etherpad_client():
+    """
+    Returns an instance of EtherpadLiteClient.
+    """
     return EtherpadLiteClient(
         base_url=settings.ETHERPAD_URL + '/api',
         api_version='1.2.7',
         base_params={ 'apikey': settings.ETHERPAD_KEY })
 
-def open_etherpad(pad, author, validUntil, e=None, **kwargs):
-    if not e:
-        e = get_etherpad_client()
-    data = e.createSession(
+def open_etherpad(pad, author, validUntil, etherpad=None, **kwargs):
+    """
+    Opens an etherpad-lite document. The etherpad client is used
+    to get a cookie for a session on the pad.
+    """
+    if not etherpad:
+        etherpad = get_etherpad_client()
+    data = etherpad.createSession(
         groupID=pad.groupid,
         authorID=author.etherpad_id,
-        validUntil=validUntil)
+        validUntil=validUntil
+    )
     response = jsonify(
         success=True,
         pad='%s/p/%s' % (settings.ETHERPAD_URL, pad.padid),
         title=pad.title,
-        **kwargs)
+        **kwargs
+    )
     set_cookie(response, 'sessionID', data['sessionID'])
     return response
