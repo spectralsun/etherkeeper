80d1f16c022c7804f772784d57264a1b8a3179f2
spectralsun <garrett@spectralsun.com>
2015-01-25 19:45:36 -0800

    urls: add grappelli

--- etherkeeper/urls.py
+++ etherkeeper/urls.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',      
+    url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^etherpad', include('etherkeeper.etherpad.urls')),
     url('', include('etherkeeper.core.urls')) 
