41f1c8b8840f8af4636065b548abdf8f1a1b9f0a
spectralsun <garrett@spectralsun.com>
2015-01-31 15:54:34 -0800

    organize models: create document, move member and invite

--- etherkeeper/etherpad/models.py
+++ etherkeeper/etherpad/models.py
@@ -1,13 +1,15 @@
 from datetime import datetime
 from django.db import models
 from etherkeeper.core.models import Author
-from etherkeeper.organize.models import Folder, Tag
+from etherkeeper.organize.models import Document
+
 
 class Pad(models.Model):
     """
     An etherpad-lite document
     Instances are created with a padid id and a groupid
     """
+    document = models.ForeignKey(Document, unique=True)
     padid = models.CharField(max_length=42)
     groupid = models.CharField(max_length=42)
     title = models.CharField(max_length=255, default='')
@@ -36,39 +38,9 @@ class Pad(models.Model):
         return ordered
 
     def get_title(self):
-        'Returns the title of a pad. If not set, returns "Untitled Document"'
+        """
+        Returns the title of a pad. If not set, returns "Untitled Document".
+        """
         return 'Untitled Document' if self.title == '' else self.title
 
-class PadMember(models.Model):
-    ROLES = (
-        ('owner', 'Owner'),
-        ('admin', 'Admin'),
-        ('write', 'Write'),
-        ('read', 'Read')
-    )
-    pad = models.ForeignKey(Pad, related_name='members')
-    role = models.CharField(max_length=10, choices=ROLES)
-    joined = models.DateTimeField(default=lambda: datetime.today())
-    author = models.ForeignKey(Author, null=True, related_name='pads')
-    tags = models.ManyToManyField(Tag)
-    
-    def role_int(self, role):
-        'Converts a role into its integer index' 
-        return [x[0] for x in self.ROLES].index(role.lower())
-    
-    def check_access(self, access):
-        'Check if member has adequate access'
-        try: 
 
-            return self.role_int(access) >= self.role_int(self.role)
-        except:
-            return None
-
-class Invite(models.Model):
-    pad = models.ForeignKey(Pad, related_name='invites')
-    sender = models.ForeignKey(Author, related_name='sent_invites')
-    to = models.ForeignKey(Author, related_name='invites')
-    #message = models.TextField(null=True)
-    role = models.CharField(max_length=10, choices=PadMember.ROLES)
-    sent = models.DateTimeField(default=lambda: datetime.today())
-    #seen = models.BooleanField(default=False)
--- etherkeeper/organize/models.py
+++ etherkeeper/organize/models.py
@@ -1,11 +1,60 @@
 from django.db import models
 from etherkeeper.core.models import Author
 
-class Folder(models.Model):
-    name = models.CharField(max_length=255)
-    parent = models.ForeignKey('Folder')
-    author = models.ForeignKey(Author, null=True)
 
+class Document(models.Model):
+    """
+    An abstract document model.
+    """
+    TYPES = (
+        ('etherpad','ep'),
+        ('ethercalc', 'ec')
+    )
+    type = models.CharField(max_length=10, choices=TYPES)
+    creator = models.ForeignKey(Author, related_name='owned_documents')
+
+class DocumentMember(models.Model):
+    """
+    A member of a Document.
+    """
+    ROLES = (
+        ('owner', 'Owner'),
+        ('admin', 'Admin'),
+        ('write', 'Write'),
+        ('read', 'Read')
+    )
+    document = models.ForeignKey(Document, related_name='members')
+    role = models.CharField(max_length=10, choices=ROLES)
+    joined = models.DateTimeField(default=lambda: datetime.today())
+    author = models.ForeignKey(Author, null=True, related_name='documents')
+    tags = models.ManyToManyField(Tag, related_name='documents')
+    
+    def role_int(self, role):
+        """
+        Converts a role into its integer index
+        """
+        return [x[0] for x in self.ROLES].index(role.lower())
+    
+    def check_access(self, access):
+        """
+        Check if member has adequate access
+        """
+        try: 
+            return self.role_int(access) >= self.role_int(self.role)
+        except:
+            return None
+
+class DocumentInvite(models.Model):
+    """
+    An invitation to become a DocumentMember.
+    """
+    document = models.ForeignKey(Document, related_name='invites')
+    sender = models.ForeignKey(Author, related_name='sent_invites')
+    to = models.ForeignKey(Author, related_name='invites')
+    message = models.TextField(null=True)
+    role = models.CharField(max_length=10, choices=PadMember.ROLES)
+    sent = models.DateTimeField(default=lambda: datetime.today())
+    #seen = models.BooleanField(default=False)    
 
 class Tag(models.Model):
     name = models.CharField(max_length=255)
